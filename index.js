5757a156be37ff7737655b436cabf33100f26158
steven chen <schen0096@gmail.com>
2021-06-16 00:54:30 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = "<h1 id = 'victory'>Steven is the champion</h1>"
